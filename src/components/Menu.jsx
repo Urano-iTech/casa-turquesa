8a12652e22d355d45a3a4bcf21660d035e4f1f75
iUranoO <uranoitech@gmail.com>
2023-08-24 00:05:54 -0400

    neo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -24,7 +24,7 @@ function Menu() {
             <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
             <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
           </div>
-          <ul class="py-2" aria-labelledby="user-menu-button">
+          <ul className="py-2" aria-labelledby="user-menu-button">
             <li>
               <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
             </li>
